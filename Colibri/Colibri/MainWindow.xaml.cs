ebeec82a937d18f7b0a8752f1a04a6bdf812368a
Biglaughingbear <51011229+Biglaughingbear@users.noreply.github.com>
2020-11-01 17:13:27 -0600

    Nov 1 2020 Master
    Added colouring and 2 new columns

---
+++ Colibri/Colibri/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Colibri
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Driver dr = new Driver();
+            if ((!string.IsNullOrEmpty(ResultSheetLoc.Text))&&(!string.IsNullOrEmpty(TransSheetLoc.Text)) &&(!string.IsNullOrEmpty(OrderSheetLoc.Text)))
+            {
+                if(dr.run(OrderSheetLoc.Text,TransSheetLoc.Text,ResultSheetLoc.Text))
+                    System.Windows.Forms.MessageBox.Show("Success");
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("One or more fields missing");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    OrderSheetLoc.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            using (System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                { 
+                    //Get the path of specified file
+                    TransSheetLoc.Text = openFileDialog.FileName;                   
+                }
+            }
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            // Show the FolderBrowserDialog.  
+            DialogResult result = folderDlg.ShowDialog();
+            if (result==System.Windows.Forms.DialogResult.OK)
+            {
+                ResultSheetLoc.Text = folderDlg.SelectedPath;
+            }
+        }
+    }
+}
---
+++ Colibri/Colibri/School.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Colibri
+{
+    class School
+    {
+        string schoolName;
+        Dictionary<int, double> feeDict;
+        Dictionary<int, double> amountDict;
+        Dictionary<int, double> netDict;
+        List<int> orderNums;
+        List<Item> itemsList;
+
+        public School(string schoolName)
+        {
+            this.schoolName = schoolName;
+            feeDict = new Dictionary<int, double>();
+            amountDict = new Dictionary<int, double>();
+            netDict = new Dictionary<int, double>();
+            itemsList = new List<Item>();
+
+            orderNums = new List<int>();
+        }
+        public void AddMoney(int orderNum, double net, double fee, double amount)
+        {
+            netDict[orderNum]=net;
+            feeDict[orderNum]= fee;
+            amountDict[orderNum]= amount;
+        }
+        public void AddItem(string name, int sku, int quant, string size)
+        {
+            bool original = true;
+            foreach (Item item in itemsList)
+            {
+                if (sku == item.SKU)
+                {
+                    item.Quant=quant;
+                    original = false;
+                    break;
+                }
+            }
+            if (original) { 
+                itemsList.Add(new Item(name, sku, quant, size));
+            }
+        }
+        public void AddOrderNum(int orderNum)
+        {
+            orderNums.Add(orderNum);
+        }
+        public bool HasOrder(int target)
+        {
+            bool result = false;
+
+            foreach (int num in orderNums)
+            {
+                if (target==num)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+        public double[] GetAllTotals()
+        {
+            return new double[] { GetTotal(netDict), GetTotal(feeDict), GetTotal(amountDict), feeDict.Keys.Count };
+        }
+        private double GetTotal(Dictionary<int, double> dict)
+        {
+            double result = 0;
+            foreach (double value in dict.Values)
+            {
+                result += value;
+            }
+            return result;
+        }
+        public Dictionary<int, double[]> GetMoney()
+        {
+            //feeDict.OrderBy(x=>x.Key);
+            Dictionary<int, double[]> results = new Dictionary<int, double[]>();
+            foreach (int key in feeDict.Keys)
+            {
+                results[key] = (new double[] { feeDict[key], netDict[key], amountDict[key] });
+            }
+            
+            return results;
+        }
+        public List<string[]> GetItems()
+        {
+            List<string[]> result = new List<string[]>();
+            foreach (Item item in itemsList)
+            {
+                result.Add(new string[] { item.Name, "" + item.Quant, "" + item.SKU });
+            }
+            return result;
+        }
+        public List<string[]> GetSortItems()
+        {
+            List<string[]> result = new List<string[]>();
+            foreach (Item item in itemsList.OrderBy(x => x.SKU).ToList())
+            {
+                result.Add(new string[] { item.Name, "" + item.Quant, "" + item.SKU });
+            }
+            return result;
+        }
+    }
+    class Item
+    {
+        string name;
+        string size;
+        int sku;
+        int num;
+        int quantity;
+        public Item(string name, int sku,int quantity, string size)
+        {
+            this.name = name;
+            this.sku = sku;
+            this.quantity = quantity;
+            this.num = num;
+            this.size = size;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public int SKU
+        {
+            get
+            {
+                return sku;
+            }
+        }
+       
+        public int Quant
+        {
+            set
+            {
+                quantity = quantity + value;
+            }
+            get
+            {
+                return quantity;
+            }
+
+        }
+
+        public string Size
+        {
+            get
+            {
+                return size;
+            }
+        }
+    }
+}
